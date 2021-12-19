5c689f66939dce1b1b8603f39b3363f01b04d858
Marcel Arie <googlillo@gmail.com>
2021-12-19 19:26:36 +0100

    build base source and try to Pipe plenary Jobs

---
+++ after/plugin/cmp_package.vim
@@ -0,0 +1,1 @@
+lua require'cmp'.register_source('package', require'cmp_package'.new())
---
+++ lua/cmp_package/init.lua
@@ -0,0 +1,84 @@
+local source = {}
+local Job = require("plenary.job")
+
+---Source constructor.
+source.new = function()
+	local self = setmetatable({}, { __index = source })
+	self.your_awesome_variable = 1
+	return self
+end
+
+---Return the source name for some information.
+source.get_debug_name = function()
+	return "package"
+end
+
+---Return the source is available or not.
+---@return boolean
+function source:is_available()
+	return true
+end
+
+---Invoke completion (required).
+---  If you want to abort completion, just call the callback without arguments.
+---@param request  cmp.CompletionRequest
+---@param callback fun(response: lsp.CompletionResponse|nil)
+function source:complete(request, callback)
+	local cur_line = request.context.cursor_line
+	local query = string.match(cur_line, '%s*"([^"]*)"?')
+	if not query then
+		return callback({})
+	end
+
+	local its_repo = string.match(query, "/")
+
+	query = string.gsub(query, "%s+", "")
+	if not query then
+		return callback({})
+	end
+
+	local url = its_repo and string.format("https://api.github.com/search/repositories?q=%s&per_page=10", query)
+		or string.format("https://api.github.com/search/users?q=%s&per_page=10", query)
+
+	Job
+		:new({
+			command = "curl",
+			args = { url },
+		} .. {
+			command = "jq",
+			args = { "'.items'" }, -- , "curl", "|", "jq", "'.items'" },
+			on_exit = function(job)
+				local res = job:result()
+				print(vim.inspect(res))
+				-- local tab = vim.json.decode(res.body)
+				--
+				-- if not tab or not tab.items then
+				-- 	return callback({})
+				-- end
+				--
+				-- local packs = {}
+				-- for _, pack in ipairs(tab.items) do
+				-- 	local name = its_repo and pack.full_name or pack.html_url
+				-- 	table.insert(packs, { label = name, insertText = name })
+				-- end
+				-- callback(packs)
+			end,
+		})
+		:start()
+end
+
+---Resolve completion item that will be called when the item selected or before the item confirmation.
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletionItem|nil)
+function source:resolve(completion_item, callback)
+	callback(completion_item)
+end
+
+---Execute command that will be called when after the item confirmation.
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletionItem|nil)
+function source:execute(completion_item, callback)
+	callback(completion_item)
+end
+
+return source
